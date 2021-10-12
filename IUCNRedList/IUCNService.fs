dccc84e9ded4ced55b97ae885af910c06a3e4a47
Adam Eri <adam@bimspot.io>
2021-10-12 09:02:16 +0200

    Added generic request to IUCN service

--- IUCNRedList/IUCNService.fs
+++ IUCNRedList/IUCNService.fs
@@ -10,43 +10,33 @@ module IUCNService =
   let apiKey =
     "9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee"
 
-  let regionList =
+  let private request<'T> token url =
     async {
-      let! response =
-        Http.AsyncRequestString(
-          "http://apiv3.iucnredlist.org/api/v3/region/list",
-          query = [ "token", apiKey ],
-          silentHttpErrors = true
-        )
-
-      let config =
-        JsonConfig.create (jsonFieldNaming = Json.snakeCase)
-
-      let result =
-        Json.deserializeEx<RegionListResponse> config response
-
-      return result
-    }
-
-  let species region =
-    async {
-      let (RegionIdentifier identifier) = region.Identifier
-
-      let url =
-        $"http://apiv3.iucnredlist.org/api/v3/species/region/{identifier}/page/0"
-
       let! response =
         Http.AsyncRequestString(
           url,
-          query = [ "token", apiKey ],
+          query = [ "token", token ],
           silentHttpErrors = true
         )
 
       let config =
         JsonConfig.create (jsonFieldNaming = Json.snakeCase)
 
-      let result =
-        Json.deserializeEx<SpeciesListResponse> config response
+      let result = Json.deserializeEx<'T> config response
 
       return result
     }
+
+  let private authorizedRequest<'T> = request<'T> apiKey
+
+  let regionList =
+    authorizedRequest<RegionListResponse>
+      "http://apiv3.iucnredlist.org/api/v3/region/list"
+
+  let species region =
+    let (RegionIdentifier identifier) = region.Identifier
+
+    let url =
+      $"http://apiv3.iucnredlist.org/api/v3/species/region/{identifier}/page/0"
+
+    authorizedRequest<SpeciesListResponse> url
